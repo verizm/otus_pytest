2e6f6cc561382cbafdd168b0f9532c3806d8103a
lazunina <veralazu@gmail.com>
2019-09-02 15:15:01 +0300

    homework6

---
+++ 6/test_products.py
@@ -0,0 +1,31 @@
+def test_add_product(get_options_driver_fixture_admin_page):
+    """
+    Проверяем вход под учеткой админа с валидными логином и паролем
+    :param get_base_url_fixture:
+    :param get_options_driver_fixture:
+    :return:
+    """
+    driver = get_options_driver_fixture_admin_page
+    assert driver
+
+
+def test_change_product(get_options_driver_fixture_admin_page):
+    """
+    Проверяем вход под учеткой админа с валидными логином и паролем
+    :param get_base_url_fixture:
+    :param get_options_driver_fixture:
+    :return:
+    """
+    driver = get_options_driver_fixture_admin_page
+    assert driver
+
+
+def test_remove_product(get_options_driver_fixture_admin_page):
+    """
+    Проверяем вход под учеткой админа с валидными логином и паролем
+    :param get_base_url_fixture:
+    :param get_options_driver_fixture:
+    :return:
+    """
+    driver = get_options_driver_fixture_admin_page
+    assert driver
--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 import time
 import pytest
 from selenium import webdriver
+from locators_opencart import AdminPage
 
 
 def pytest_addoption(parser):
@@ -49,6 +50,21 @@ def create_driver(request, browser, is_headless=True, sleep_time=0):
     return driver
 
 
+def login_opencart_admin(base_url, driver):
+    driver.get(base_url + "admin/")
+    search_input_login = driver.find_element_by_id(AdminPage.admin_login_field)
+    search_input_login.send_keys(AdminPage.keys_login)
+    search_input_password = driver.find_element_by_id(AdminPage.admint_password_field)
+    search_input_password.send_keys(AdminPage.keys_password)
+    search_button = driver.find_element_by_class_name(AdminPage.enter_admin_button)
+    search_button.click()
+
+def get_prodact_page(driver):
+    search_catalog_button = driver.find_element_by_id("menu-catalog")
+    search_catalog_button.click()
+    search_product_button = driver.find_element_by_css("active")
+
+
 @pytest.fixture
 def get_options_driver_fixture(request):
     """
@@ -68,3 +84,19 @@ def get_parametrize_drivers_fixture(request):
     """
     browser_param = request.param
     return create_driver(request, browser_param)
+
+
+@pytest.fixture
+def get_options_driver_fixture_admin_page(request):
+    """
+    Фикстура возвращает драйвер
+    """
+    browser = request.config.getoption('--browser')
+    driver = create_driver(request, browser, False, 5)
+    login_opencart_admin(request.config.getoption('--url'), driver)
+    search_catalog_button = driver.find_element_by_id("menu-catalog")
+    search_catalog_button.click()
+    search_product_button = driver.find_element_by_class_name("active")
+    search_product_button.click()
+
+    return driver
